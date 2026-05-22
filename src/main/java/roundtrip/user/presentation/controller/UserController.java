4d398938509a0b66e75988b33b0d313e5073a1a5
kimminje <kmz34180@naver.com>
2026-05-22 14:51:23 +0900

    refactor(user, auth): 컨트롤러 패키지 컨벤션 통일

--- src/main/java/roundtrip/user/presentation/controller/UserController.java
+++ src/main/java/roundtrip/user/presentation/controller/UserController.java
@@ -1,4 +1,4 @@
-package roundtrip.user.presentation;
+package roundtrip.user.presentation.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
